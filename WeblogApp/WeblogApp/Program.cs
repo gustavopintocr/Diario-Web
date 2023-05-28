e0bcc73b6f7e55755348ff184be2af1bee6496b3
Mafe Andres <mafe.andres.monge@gmail.com>
2023-05-27 22:26:20 -0600

    Initial solutions installed efcore

--- WeblogApp/WeblogApp/Program.cs
+++ WeblogApp/WeblogApp/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<MvcMovieContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext")));
 
 var app = builder.Build();
 
