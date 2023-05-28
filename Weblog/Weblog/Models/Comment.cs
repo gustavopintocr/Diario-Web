a065b5ee0f8b4f35ce1a339daf6449263a987bf7
Gustavo Pinto Umaña <GUSTAVO.PINTO@ucr.ac.cr>
2023-05-27 22:53:11 -0600

    Models

---
+++ Weblog/Weblog/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Weblog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string? Body { get; set; }
+        public string? Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int IdPublication { get; set; }
+        public Publication Publication { get; set; } = null!;
+    }
+}
---
+++ Weblog/Weblog/Models/Publication.cs
@@ -0,0 +1,16 @@
+﻿namespace Weblog.Models
+{
+    public class Publication
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime Date { get; set; }
+        public string? Header { get; set; }
+        public byte[]? Image { get; set; }
+        public string? Body { get; set; }
+        public int NumComments { get; set; }
+        public int IdAuthor { get; set; }
+        public Author Author { get; set; } = null!;
+
+    }
+}
