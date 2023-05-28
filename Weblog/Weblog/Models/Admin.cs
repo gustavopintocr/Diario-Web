42f1b482fa1b3c02cbd1420f34e4db42c558cf2c
Mafe Andres <mafe.andres.monge@gmail.com>
2023-05-27 22:45:37 -0600

    Added user, admin, author

---
+++ Weblog/Weblog/Models/Admin.cs
@@ -0,0 +1,9 @@
+﻿namespace Weblog.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
---
+++ Weblog/Weblog/Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace Weblog.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+        public ICollection<Publication> Publications { get;} = new List<Publication>();
+    }
+}
